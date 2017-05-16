54bde76ee7b685e441fef0cf5118e00b25a00cd2
ZinMar91 <zinmar91prettygirl@gmail.com>
2017-05-16 18:39:37 +0630

    Register + Login + Logout

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,11 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('users/register', 'Auth\RegisterController@show');
+Route::post('users/register', 'Auth\RegisterController@register');
+
+Route::get('users/logout', 'Auth\LoginController@logout');
+
+Route::get('users/login', 'Auth\LoginController@show');
+Route::post('users/login', 'Auth\LoginController@login');
